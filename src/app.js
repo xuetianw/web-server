2c2e926bd57a85a70f3f28db3f56f107cb8d9c0d
xuetianw <xuetianw@sfu.ca>
2019-09-18 17:33:48 -0700

    add my first profolio content

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast')
 
 const app = express()
 
@@ -23,7 +21,7 @@ app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Weather',
+        title: 'Fred Wu',
         name: 'Andrew Mead'
     })
 })
@@ -35,60 +33,16 @@ app.get('/about', (req, res) => {
     })
 })
 
-app.get('/help', (req, res) => {
-    res.render('help', {
-        helpText: 'This is some helpful text.',
-        title: 'Help',
-        name: 'Andrew Mead'
+app.get('/experience', (req, res) => {
+    res.render('experience', {
+        title: 'experience',
+        name: 'Fred Wu',
+        text: 'Software Developer Analyst\n' +
+            'Sept 2017– Dec 2017\n' +
+            'BCAA - Burnaby, BC\n'
     })
 })
 
-app.get('/weather', (req, res) => {
-    if (!req.query.address) {
-        return res.send({
-            error: 'You must provide an address!'
-        })
-    }
-
-    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
-        if (error) {
-            return res.send({ error })
-        }
-
-        forecast(latitude, longitude, (error, forecastData) => {
-            if (error) {
-                return res.send({ error })
-            }
-
-            res.send({
-                forecast: forecastData,
-                location,
-                address: req.query.address
-            })
-        })
-    })
-})
-
-app.get('/products', (req, res) => {
-    if (!req.query.search) {
-        return res.send({
-            error: 'You must provide a search term'
-        })
-    }
-
-    console.log(req.query.search)
-    res.send({
-        products: []
-    })
-})
-
-app.get('/help/*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        name: 'Andrew Mead',
-        errorMessage: 'Help article not found.'
-    })
-})
 
 app.get('*', (req, res) => {
     res.render('404', {
