f67d24cd2917b6dfba026080ef3dde1837db3acb
xuetianw <xuetianw@sfu.ca>
2019-09-25 00:30:02 -0700

    fix title and fix resume http request

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const express = require('express')
-const hbs = require('hbs')
+const fs = require('fs');
 
 const app = express()
 
@@ -8,13 +8,6 @@ const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
-
-// Setup handlebars engine and views location
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
@@ -23,3 +16,9 @@ app.listen(port, () => {
     console.log('Server is up on port .' + port)
 })
 
+app.get('/about', (req, res) => {
+    var data =fs.readFileSync(`${__dirname}/../resume/Resume.pdf`);
+    res.contentType("application/pdf");
+    res.send(data);
+
+})
