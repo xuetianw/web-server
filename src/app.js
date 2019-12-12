46b980a35bc8cc031ec52476ee8a19fd58edd162
xuetianw <xuetianw@sfu.ca>
2019-12-12 15:34:33 -0800

    add leetcode account and project link

--- src/app.js
+++ src/app.js
@@ -1,24 +1,24 @@
-const path = require('path')
-const express = require('express')
+const path = require('path');
+const express = require('express');
 const fs = require('fs');
 
-const app = express()
+const app = express();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
-const publicDirectoryPath = path.join(__dirname, '../public')
+const publicDirectoryPath = path.join(__dirname, '../public');
 
 // Setup static directory to serve
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
 app.listen(port, () => {
     console.log('Server is up on port .' + port)
-})
+});
 
 app.get('/resume', (req, res) => {
     var data =fs.readFileSync(`${__dirname}/../resume/resume.pdf`);
     res.contentType("application/pdf");
     res.send(data);
 
-})
+});
