ccb6270b9e731ced7e85a5c77ee15ee91569d93a
xuetianw <xuetianw@sfu.ca>
2020-03-21 21:32:55 -0700

    add JQuery and co-op documents

---
+++ public/Javacript/resume.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $( "#Work-term-details").click(function() {
+        location.href = "../documents/job-details.pdf";
+    });
+
+    $( "#supervisor-evaluation").click(function() {
+        location.href = "../documents/Supervisor-Evaluation.pdf";
+    });
+});
+
